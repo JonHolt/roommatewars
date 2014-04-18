9860da1bb435d259b0ffe422934c7eb650e0a9d8
JonHolt <jonathanholt92@gmail.com>
2014-04-18 10:13:23 -0600

    giving mike the Gruntfile for free cause he's a bitch

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = function(grunt){
+    require('matchdep').filter('grunt-*').forEach(grunt.loadNpmTasks);
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        watch: {
+            debug:{
+                files: 'client/main.js',
+                tasks: ['browserify:debug']
+            },
+            release:{
+                files: 'client/main.js',
+                tasks: ['browserify:release']
+            }
+        },
+        browserify: {
+            debug: {
+               files: {
+                   'client/build/game-debug.js': ['client/main.js']
+               },
+               options:{
+                   debug:true
+               }
+            },
+            release: {
+               files: {
+                   'client/build/game.js': ['client/main.js']
+               },
+               options:{
+                   debug:false
+               }
+            }
+        }
+    });
+}
