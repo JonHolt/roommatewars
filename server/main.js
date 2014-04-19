c711e580e1ebdb677f8b3cb4035f689b6c8c4dc4
MCluck <mcluck90@gmail.com>
2014-04-19 13:13:11 -0600

    Got basic server connection running

--- server/main.js
+++ server/main.js
@@ -1,2 +1,22 @@
 'use strict';
 
+var World = require('psykick2d').World;
+
+World.init({
+    width: 900,
+    height: 600,
+    serverMode: true
+});
+
+var io = require('socket.io').listen(4242);
+io.set('authorization', function (handshakeData, cb) {
+    console.log(handshakeData.query);
+    cb(null, true);
+});
+io.sockets.on('connection', function(socket) {
+    console.log('Connected');
+    socket.emit('confirm', {
+        name: 'Jon',
+        occupation: 'Badass'
+    });
+});
